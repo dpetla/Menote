a27eed34d5365aca55f8404cfc1b32c356719d83
Douglas Petla <dpetla@gmail.com>
2018-08-18 22:15:01 -0400

    removing profile component

--- src/app/settings/settings-routing.module.ts
+++ src/app/settings/settings-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../auth/auth-guard.service';
 import { AccountComponent } from './account/account.component';
-import { ProfileComponent } from './profile/profile.component';
 import { SettingsComponent } from './settings.component';
 
 const settingsRoutes: Routes = [
@@ -10,8 +9,7 @@ const settingsRoutes: Routes = [
     path: '',
     component: SettingsComponent,
     children: [
-      { path: 'account', component: AccountComponent },
-      { path: 'profile', component: ProfileComponent }
+      { path: 'account', component: AccountComponent }
     ],
     canActivate: [AuthGuard]
   }
--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -2,13 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountComponent } from './account/account.component';
-import { ProfileComponent } from './profile/profile.component';
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './settings.component';
 
 @NgModule({
-  declarations: [SettingsComponent, AccountComponent, ProfileComponent],
+  declarations: [SettingsComponent, AccountComponent],
   imports: [CommonModule, FormsModule, SettingsRoutingModule],
-  exports: [SettingsRoutingModule, AccountComponent, ProfileComponent]
+  exports: [SettingsRoutingModule, AccountComponent]
 })
 export class SettingsModule {}

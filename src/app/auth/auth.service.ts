fc6db9a64b49a5905cd3a1ec776d186b632be9fb
Douglas Petla <dpetla@gmail.com>
2018-08-28 23:25:01 -0400

    refactoring auth with google sign in

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,80 +1,53 @@
 import { Injectable, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Injectable()
 export class AuthService implements OnInit, OnDestroy {
   token: string;
   auth: any;
   user: any;
-  userFB: Observable<firebase.User>;
   subscription: Subscription;
 
-  constructor(private router: Router, private afAuth: AngularFireAuth) {
-    this.user = afAuth.authState;
-  }
+  constructor(private router: Router) {}
 
   ngOnInit() {
-    // this.auth = firebase.auth();
-  }
-
-  async isLoggedIn() {
-    return (await this.user) !== null;
+    firebase.auth().onAuthStateChanged(user => (this.user = user));
   }
 
   loginWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
-    this.afAuth.auth
+    firebase
+      .auth()
       .signInWithPopup(provider)
       .then(result => {
-        result.user.getIdToken().then(token => (this.token = token));
-        this.subscription = this.userFB.subscribe(user => {
-          this.user = user;
-          console.log(this.user);
-        });
+        this.user = result.user;
+        if (this.user) {
+          this.user.getIdToken().then(token => localStorage.setItem('menote-user-token', token));
+        } else {
+          localStorage.removeItem('menote-user-token');
+        }
+
         this.router.navigate(['/notes']);
       })
       .catch(error => console.log(`Error while logging in. ERROR: ${error}`));
   }
 
   logout() {
-    this.afAuth.auth
+    firebase
+      .auth()
       .signOut()
-      .then(result => this.router.navigate(['/']))
+      .then(result => {
+        this.user = null;
+        localStorage.removeItem('menote-user-token');
+        this.router.navigate(['/']);
+      })
       .catch(error => console.log(`Error while logging out. ERROR: ${error}`));
   }
 
-  // login() {
-  //   const provider = new firebase.auth.GoogleAuthProvider();
-  //   provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-  //   provider.addScope('profile');
-  //   provider.addScope('email');
-  //   firebase
-  //     .auth()
-  //     .signInWithPopup(provider)
-  //     .then(result => {
-  //       console.log(result);
-  //       result.user.getIdToken().then(token => (this.token = token));
-  //       this.user = result.user;
-  //       this.router.navigate(['/notes']);
-  //     });
-  // }
-
-  // logout() {
-  //   firebase
-  //     .auth()
-  //     .signOut()
-  //     .then(() => {
-  //       console.log('signout completed ...');
-  //       this.router.navigate(['/']);
-  //     })
-  //     .catch(error => console.log(error));
-  // }
-
   isAuthenticated() {
-    return this.token != null;
+    return this.user != null || !!localStorage.getItem('menote-user-token');
   }
 
   ngOnDestroy() {

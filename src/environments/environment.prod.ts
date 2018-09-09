b08c38bb9f51d1dbf084047116833e2731999d15
Douglas Petla <dpetla@gmail.com>
2018-09-08 22:41:53 -0400

    adding environments back

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBcC9pbQu9Xb6nmfs5WhpSB_1aWX6syQdU',
+    authDomain: 'ng-journal-app.firebaseapp.com',
+    databaseURL: 'https://ng-journal-app.firebaseio.com',
+    projectId: 'ng-journal-app',
+    storageBucket: 'ng-journal-app.appspot.com',
+    messagingSenderId: '768544417123'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBcC9pbQu9Xb6nmfs5WhpSB_1aWX6syQdU',
+    authDomain: 'ng-journal-app.firebaseapp.com',
+    databaseURL: 'https://ng-journal-app.firebaseio.com',
+    projectId: 'ng-journal-app',
+    storageBucket: 'ng-journal-app.appspot.com',
+    messagingSenderId: '768544417123'
+  }
+};

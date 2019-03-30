c01e4be42b78790215d0cf496641b4a0c5605f20
Douglas Petla <dpetla@gmail.com>
2019-03-29 21:24:40 -0400

    refactor notes service

--- src/app/notes/notes.service.ts
+++ src/app/notes/notes.service.ts
@@ -5,10 +5,19 @@ import {
   AngularFirestoreCollection
 } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { LocalInfoService } from '../shared/local-info.service';
+import { WeatherApiResponse } from '../shared/WeatherApiResponse.model';
 import { AuthService } from './../auth/auth.service';
 import * as noteTemplate from './note-templates';
 import { Note } from './note.model';
+
+interface apiData {
+  weatherDesc: string;
+  temp: string;
+  city: string;
+  country: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +25,6 @@ import { Note } from './note.model';
 export class NotesService {
   notesRef: AngularFirestoreCollection<Note>;
   notes$: Observable<Note[]>;
-  deletedNotesRef: AngularFirestoreCollection<Note[]>;
   newNote: Note = noteTemplate.newNote;
 
   constructor(
@@ -25,14 +33,27 @@ export class NotesService {
     private authService: AuthService,
     private router: Router
   ) {
+    this.notesRef = this.getNotesRef();
+
+    this.notes$ = this.notesRef
+      .snapshotChanges()
+      .pipe(map(this.convertPayloadToNotes));
+
     this.createWelcomeNoteForNewUser();
   }
 
   getNotesRef(): AngularFirestoreCollection<Note> {
-    return this.db.collection('notes', ref => {
-      return ref
-        .where('uid', '==', this.authService.user.uid)
-        .orderBy('dateCreated', 'desc');
+    const uid = localStorage.getItem('menote-uid') || this.authService.user.uid;
+    return this.db.collection('notes', ref =>
+      ref.where('uid', '==', uid).orderBy('dateCreated', 'desc')
+    );
+  }
+
+  convertPayloadToNotes(actions) {
+    return actions.map(action => {
+      const data = action.payload.doc.data();
+      const id = action.payload.doc.id;
+      return { id, ...data } as Note;
     });
   }
 
@@ -50,31 +71,44 @@ export class NotesService {
     return this.notesRef.doc(id);
   }
 
-  // get location data then gather data to create note
   createNote() {
-    this.localInfoService.getLocalInfo(() => {
-      // enter note data
-      this.newNote.uid = this.authService.user.uid;
-      this.newNote.location = `${this.localInfoService.city}, ${
-        this.localInfoService.country
-      }`;
-      this.newNote.title = new Date().toDateString();
-      this.newNote.dateCreated = new Date();
-      this.newNote.weather = `${this.localInfoService.temp} ${
-        this.localInfoService.weatherDesc
-      }`;
+    this.localInfoService
+      .getLocalInfo()
+      .pipe(
+        map((data: WeatherApiResponse) => {
+          return {
+            weatherDesc: data.weather[0].description,
+            temp: this.formatTempString(data.main.temp),
+            city: data.name,
+            country: data.sys.country
+          };
+        })
+      )
+      .subscribe((apiData: apiData) => this.populateInitialFields(apiData));
+  }
 
-      // create instruction note when signing up new user
-      if (this.authService.isNewUser) {
-        this.newNote.title = 'Welcome to menote!';
-        this.newNote.content = noteTemplate.firstNoteContent;
-      }
+  formatTempString(temp: number): string {
+    return Math.round(temp) + String.fromCharCode(176) + 'C';
+  }
 
-      // create note document in db
-      this.notesRef
-        .add(this.newNote)
-        .then(note => this.router.navigate([note.path]))
-        .catch(error => console.log(error));
-    });
+  populateInitialFields(apiData: apiData) {
+    // enter note data
+    this.newNote.uid = this.authService.user.uid;
+    this.newNote.location = `${apiData.city}, ${apiData.country}`;
+    this.newNote.title = new Date().toDateString();
+    this.newNote.dateCreated = new Date();
+    this.newNote.weather = `${apiData.temp} ${apiData.weatherDesc}`;
+
+    // create instruction note when signing up new user
+    if (this.authService.isNewUser) {
+      this.newNote.title = 'Welcome to menote!';
+      this.newNote.content = noteTemplate.firstNoteContent;
+    }
+
+    // create note document in db
+    this.notesRef
+      .add(this.newNote)
+      .then(note => this.router.navigate([note.path]))
+      .catch(error => console.log(error));
   }
 }

e150d703fd8c10aa5cb1be5b68e818c546d2e976
Douglas Petla <dpetla@gmail.com>
2019-11-16 21:50:24 -0500

    add ngrx/router-store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AngularFireModule } from 'angularfire2';
@@ -58,7 +59,8 @@ firebase.initializeApp({
     StoreDevtoolsModule.instrument({
       maxAge: 25,
       logOnly: environment.production
-    })
+    }),
+    StoreRouterConnectingModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,10 +1,40 @@
-import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+import * as fromRouter from '@ngrx/router-store';
+import {
+  createFeatureSelector,
+  ActionReducerMap,
+  MetaReducer
+} from '@ngrx/store';
+
 import { environment } from '../../environments/environment';
+import * as fromAuth from '../auth/store/auth.reducer';
 
-export interface State {}
+export interface AppState {
+  router: fromRouter.RouterReducerState<any>;
+  // authState: fromAuth.AuthState;
+}
 
-export const reducers: ActionReducerMap<State> = {};
+export const reducers: ActionReducerMap<AppState> = {
+  router: fromRouter.routerReducer
+  // auth: fromAuth.reducer
+};
 
-export const metaReducers: MetaReducer<State>[] = !environment.production
+export const metaReducers: MetaReducer<AppState>[] = !environment.production
   ? []
   : [];
+
+export const selectRouter = createFeatureSelector<
+  AppState,
+  fromRouter.RouterReducerState<any>
+>('router');
+
+const {
+  selectQueryParams, // select the current route query params
+  selectQueryParam, // factory function to select a query param
+  selectRouteParams, // select the current route params
+  selectRouteParam, // factory function to select a route param
+  selectRouteData, // select the current route data
+  selectUrl // select the current url
+} = fromRouter.getSelectors(selectRouter);
+
+export const selectRouteId = selectRouteParam('id');
+export const selectStatus = selectQueryParam('status');

f895e6a22d363f2bfe64602ab1933ca79795ece7
Douglas Petla <dpetla@users.noreply.github.com>
2020-02-08 11:21:05 -0500

    Ng9 update (#9)
    * update ng to v9
    
    * update about
    
    * app clean up

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -2,11 +2,8 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { createAction, props } from '@ngrx/store';
 
 export const login = createAction('[AUTH] Login');
-
 export const loginWithPopUp = createAction('[SIGN IN] Login with pop up');
-
 export const loginSuccess = createAction('[SIGN IN] Login Success', props<{ user: any; isNewUser: boolean }>());
-
 export const loginFailure = createAction('[AUTH] Login Failure', props<{ error: HttpErrorResponse }>());
 
 export const setSessionPersistence = createAction('[AUTH] Set Session Persistence');
@@ -16,13 +13,9 @@ export const setSessionPersistenceFailure = createAction(
 );
 
 export const logout = createAction('[AUTH] Logout');
-
 export const logoutSuccess = createAction('[AUTH] Logout Success');
-
 export const logoutFailure = createAction('[AUTH] Login Fail', props<{ error: HttpErrorResponse }>());
 
 export const getToken = createAction('[AUTH] Get Token');
-
 export const getTokenSuccess = createAction('[AUTH] Get Token Success', props<{ token: string }>());
-
 export const getTokenFailure = createAction('[AUTH] Get Token Failure', props<{ error: HttpErrorResponse }>());
--- src/app/types/note.interface.ts
+++ src/app/types/note.interface.ts
@@ -3,8 +3,14 @@ export interface Note {
   uid: string;
   title: string;
   content: string;
-  dateCreated: { seconds: number; nanoseconds: number };
-  dateUpdated: { seconds: number; nanoseconds: number };
+  dateCreated: {
+    seconds: number;
+    nanoseconds: number;
+  };
+  dateUpdated: {
+    seconds: number;
+    nanoseconds: number;
+  };
   location: string;
   weather: string;
   mood: string;
